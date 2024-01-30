552b1c69604757e93d924ede6afd52ce4c862377
raliwalas <rachel.aliwalas52@gmail.com>
2024-01-29 20:43:36 -0500

    brainstorming ttt classes, solutions, and other things to consider

--- tic-tac-toe/tic-tac-toe.rb
+++ tic-tac-toe/tic-tac-toe.rb
@@ -1,4 +1,41 @@
 =begin
-  brainstorming will go here
+  Board class
+    - prints the board and where the current pieces are
+  
+  1. make the game work with just me editing the hardcode - i.e. 
+    make a function that can tell whether the game is complete or not
+    - there are 8 ways to win
+    - check that row, col, or diagonal have 3 of a kind
+
+    _ _ _
+    _ _ _
+    _ _ _ 
+
+    empty board will look like this ^
+
+    wins: 
+    
+    x x x     _ _ _    _ _ _
+    _ _ _     x x x    _ _ _
+    _ _ _     _ _ _    x x x
+    
+    x _ _     _ _ x
+    _ x _     _ x _
+    _ _ x     x _ _
+
+    x _ _     _ x _     _ _ x 
+    x _ _     _ x _     _ _ x 
+    x _ _     _ x _     _ _ x 
+
+    if not of these patterns are deteched, and then return false
+
+      also need it to recognize when board is full and game is over with no winner and when 
+        game is over because there was a win
+        - maybe check that the board has no more underscores?
+
+    - split up board by row, col, diagonal???
+
+  2. add user inputs
+      - will need to brainstorm this more
 
 =end
